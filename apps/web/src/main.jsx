b6d7e28f3fe582fbc95a4979ba2531472373f76f
cuongdangcap <cuongdcoy@gmail.com>
2026-07-21 17:36:57 +0700

    Load simplified login styles

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
 import "./ux.css";
+import "./login.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
