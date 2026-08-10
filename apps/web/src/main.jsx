028df784ac2aecdd992b420fae921249a6a1c94e
cuongdangcap <cuongdcoy@gmail.com>
2026-08-10 22:33:09 +0700

    Load CMC blue theme last

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -8,6 +8,7 @@ import "./responsive-fixes.css";
 import "./redesign.css";
 import "./chat-redesign.css";
 import "./metric-help-fix.css";
+import "./cmc-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
